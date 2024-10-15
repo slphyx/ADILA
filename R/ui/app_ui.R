d3cef5b70e6b246b9f9ad2d7031b577a599a9dc4
Sompob Saralamba <sakngoi@hotmail.com>
2024-10-15 09:46:18 +0700

    update UI

--- R/ui/app_ui.R
+++ R/ui/app_ui.R
@@ -3,121 +3,83 @@ library(shiny)
 library(gt)
 library(gtools) # For the rdirichlet function
 library(shinyjs)
+library(shinydashboard)
 
-
-app_ui <- fluidPage(
+app_ui <- dashboardPage(
+  skin = "black",
+  title = "Antibiotic Usage Model",
   
-  # Use shinyjs to control visibility of elements
-  useShinyjs(),
+  # HEADER ------------------------------------------------------------------
+  dashboardHeader(
+    title = span(img(src = "logo.svg", height = 35), "Antibiotic Usage Model"),
+    titleWidth = 300
+  ),
   
-  # Navbar with three menus
-  navbarPage(
-    title = "Antibiotic Usage Model",
-    
-    # Menu 1: About App (Empty for now)
-    tabPanel("About App", 
-             h3("About this App"),
-             p("This section will describe the app in detail.")),
-    
-    # Menu 2: Run Simulation
-    tabPanel("Run Simulation",
-             
-             # Layout with two sections (above: inputs, below: output)
-             fluidRow(
-               
-               # Top section: input parameters with two tabs
-               column(12,
-                      actionButton("toggle_input", "Show/Hide Input Parameters"),
-                      hidden(
-                        div(id = "input_parameters",
-                            
-                            # Tabset for Input Parameters and Summary
-                            tabsetPanel(
-                              
-                              # Tab 1: Inputs for std_err and adult_cases$cases
-                              tabPanel("Inputs for std_err and adult_cases$cases",
-                                       
-                                       fluidRow(
-                                         column(4,
-                                                sliderInput("std_err", 
-                                                            "Standard Error (as a percentage)", 
-                                                            min = 0, max = 100, value = 20, step = 0.1)
-                                         )
-                                       ),
-                                       
-                                       fluidRow(
-                                         column(4, sliderInput("cap_cases", "CAP Cases", min = 0, max = 100, value = 50)),
-                                         column(4, sliderInput("hap_cases", "HAP Cases", min = 0, max = 100, value = 10)),
-                                         column(4, sliderInput("cns_cases", "CNS Cases", min = 0, max = 100, value = 15))
-                                       ),
-                                       
-                                       fluidRow(
-                                         column(4, sliderInput("ia_cases", "Intra-abdominal Infection Cases", min = 0, max = 100, value = 35)),
-                                         column(4, sliderInput("pye_cases", "Pyelonephritis Cases", min = 0, max = 100, value = 35)),
-                                         column(4, sliderInput("sst_cases", "Skin and Soft Tissue Infection Cases", min = 0, max = 100, value = 30))
-                                       ),
-                                       
-                                       fluidRow(
-                                         column(4, sliderInput("bj_cases", "Bone and Joint Infection Cases", min = 0, max = 100, value = 30)),
-                                         column(4, sliderInput("cdif_cases", "C. difficile Infection Cases", min = 0, max = 100, value = 30)),
-                                         column(4, sliderInput("fn_cases", "Febrile Neutropenia Cases", min = 0, max = 100, value = 10))
-                                       ),
-                                       
-                                       fluidRow(
-                                         column(4, sliderInput("sepsis_cases", "Sepsis Cases", min = 0, max = 100, value = 30)),
-                                         column(4, sliderInput("sp_cases", "Surgical Prophylaxis Cases", min = 0, max = 100, value = 60))
-                                       )
-                              ),
-                              
-                              # Tab 2: Inputs for para.data$value
-                              tabPanel("Inputs for para.data$value",
-                                       
-                                       fluidRow(
-                                         column(4, sliderInput("p_first", "Probability of first-choice antibiotics", min = 0, max = 1, value = 0.8, step = 0.01)),
-                                         column(4, sliderInput("cap_severe", "Proportion of severe CAP cases", min = 0, max = 1, value = 0.4, step = 0.01)),
-                                         column(4, sliderInput("hap_mdr", "Risk of multi-drug resistant HAP infection", min = 0, max = 1, value = 0.5, step = 0.01))
-                                       ),
-                                       
-                                       fluidRow(
-                                         column(4, sliderInput("abd_severe", "Proportion of severe intra-abdominal infection cases", min = 0, max = 1, value = 0.45, step = 0.01)),
-                                         column(4, sliderInput("uti_severe", "Proportion of severe upper UTI cases", min = 0, max = 1, value = 0.35, step = 0.01)),
-                                         column(4, sliderInput("cdf_severe", "Proportion of severe C. difficile infection cases", min = 0, max = 1, value = 0.25, step = 0.01))
-                                       ),
-                                       
-                                       fluidRow(
-                                         column(4, sliderInput("sst_nf", "Proportion of necrotizing fasciitis cases in SST patients", min = 0, max = 1, value = 0.04, step = 0.01)),
-                                         column(4, sliderInput("esbl_prevalence", "Prevalence of ESBL", min = 0, max = 1, value = 0.4, step = 0.01)),
-                                         column(4, sliderInput("mrsa_prevalence", "Prevalence of MRSA", min = 0, max = 1, value = 0.3, step = 0.01))
-                                       ),
-                                       
-                                       fluidRow(
-                                         column(4, sliderInput("strep_pyogenes", "Prevalence of Strep pyogenes infection in necrotizing fasciitis", min = 0, max = 1, value = 0.1, step = 0.01)),
-                                         column(4, sliderInput("admitted_patients", "Total admitted patients", min = 0, max = 1000, value = 600))
-                                       )
-                              ),
-                              
-                              # Tab 3: Summary of inputs
-                              tabPanel("Summary Inputs",
-                                       h4("Summary of Inputs"),
-                                       tableOutput("summary_inputs")
-                              )
-                            )
-                        )
-                      ),
-                      actionButton("run_model", "Run Model")
-               ),
-               
-               # Bottom section: Simulation output
-               column(12,
-                      h3("Summary Table of Expected Antibiotic Usage"),
-                      gt_output(outputId = "summary_table")
+  # SIDEBAR -----------------------------------------------------------------
+  dashboardSidebar(
+    width = 350,
+    useShinyjs(),
+    sidebarMenu(
+      
+      # Sidebar item for "Type of Patients"
+      menuItem("Type of patients", tabName = "type_patients", icon = icon("user"),
+               div(id = "type_patients_inputs",
+                   numericInput("cap_cases", "CAP Cases", min = 0, max = 100, value = 50),
+                   numericInput("hap_cases", "HAP Cases", min = 0, max = 100, value = 10),
+                   numericInput("cns_cases", "CNS Cases", min = 0, max = 100, value = 15),
+                   numericInput("ia_cases", "Intra-abdominal Infection Cases", min = 0, max = 100, value = 35),
+                   numericInput("pye_cases", "Pyelonephritis Cases", min = 0, max = 100, value = 35),
+                   numericInput("sst_cases", "Skin and Soft Tissue Infection Cases", min = 0, max = 100, value = 30),
+                   numericInput("bj_cases", "Bone and Joint Infection Cases", min = 0, max = 100, value = 30),
+                   numericInput("cdif_cases", "C. difficile Infection Cases", min = 0, max = 100, value = 30),
+                   numericInput("fn_cases", "Febrile Neutropenia Cases", min = 0, max = 100, value = 10),
+                   numericInput("sepsis_cases", "Sepsis Cases", min = 0, max = 100, value = 30),
+                   numericInput("sp_cases", "Surgical Prophylaxis Cases", min = 0, max = 100, value = 60)
                )
-             )
-    ),
-    
-    # Menu 3: Model Details (Empty for now)
-    tabPanel("Model Details", 
-             h3("Model Details"),
-             p("This section will explain the details of the model."))
+            
+      ),
+      
+      # Sidebar item for "Severity of Cases"
+      menuItem("Severity of cases", tabName = "severity_cases", icon = icon("exclamation-triangle"),
+               div(id = "severity_cases_inputs",
+                   numericInput("cap_severe", "Proportion of severe CAP cases", min = 0, max = 1, value = 0.4, step = 0.01),
+                   numericInput("abd_severe", "Proportion of severe intra-abdominal infection cases", min = 0, max = 1, value = 0.45, step = 0.01),
+                   numericInput("uti_severe", "Proportion of severe upper UTI cases", min = 0, max = 1, value = 0.35, step = 0.01),
+                   numericInput("cdf_severe", "Proportion of severe C. difficile infection cases", min = 0, max = 1, value = 0.25, step = 0.01),
+                   numericInput("sst_nf", "Proportion of necrotizing fasciitis cases in SST patients", min = 0, max = 1, value = 0.04, step = 0.01)
+               )
+               
+      ),
+      
+      # Sidebar item for "Availability of First Choice Antibiotics"
+      menuItem("Availability of first choice antibiotics", tabName = "availability_antibiotics", icon = icon("capsules"),
+               div(id = "availability_antibiotics_inputs",
+                   numericInput("p_first", "Probability of first-choice antibiotics", min = 0, max = 1, value = 0.8, step = 0.01)
+               )           
+      ),
+      
+      # Sidebar item for "Prevalence of AMR"
+      menuItem("Prevalence of AMR", tabName = "prevalence_amr", icon = icon("chart-line"),
+               div(id = "prevalence_amr_inputs",
+                   numericInput("esbl_prevalence", "Prevalence of ESBL", min = 0, max = 1, value = 0.4, step = 0.01),
+                   numericInput("mrsa_prevalence", "Prevalence of MRSA", min = 0, max = 1, value = 0.3, step = 0.01),
+                   numericInput("strep_pyogenes", "Prevalence of Strep pyogenes infection in necrotizing fasciitis", min = 0, max = 1, value = 0.1, step = 0.01)
+               )         
+      ),
+      
+      # Sidebar item for "Total Admitted Patients"
+      menuItem("Total admitted patients", tabName = "total_patients", icon = icon("hospital")),
+      div(id = "total_patients_inputs",
+          numericInput("admitted_patients", "Total admitted patients", min = 0, max = 1000, value = 600)
+      )
+    )
+  ),
+  
+  # BODY --------------------------------------------------------------------
+  dashboardBody(
+    fluidRow(
+      column(12, h3("Select inputs from the sidebar to configure the model"))
+    )
   )
 )
+
