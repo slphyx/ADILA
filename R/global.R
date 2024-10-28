383b4d14dbbd2cc4656affc5e68d954d16d23ef0
Sompob Saralamba <Sompob@tropmedres.ac>
2024-10-28 15:06:56 +0700

    change header of first choice table

--- R/global.R
+++ R/global.R
@@ -6,7 +6,7 @@ source("R/functions/data.adult.shiny.v3.R")
 source("R/functions/generate_input_dataframe.R")
 source("R/functions/fluid_design.R")
 
-first_choice_list <- data.frame(First_choice=c("Amoxicillin",
+first_choice_list <- data.frame("First-choice antibiotics (WHO AwaRe book)"=c("Amoxicillin",
                                                "Amoxicillin/ clavulanic-acid",
                                                "Phenoxymethyl penicillin",
                                                "Ampicillin",
@@ -27,6 +27,6 @@ first_choice_list <- data.frame(First_choice=c("Amoxicillin",
                                                "Piperacillin + Tazobactam",
                                                "Clarithromycin",
                                                "Vancomycin",
-                                               "Ciprofloxacin"))
+                                               "Ciprofloxacin"),check.names = FALSE)
 
 value_fin <- reactiveValues(finished=0, TotalAdmittedPatient=0)   # for checking whether the simulation has finished ; 0 <- not finished 1 <- finished
